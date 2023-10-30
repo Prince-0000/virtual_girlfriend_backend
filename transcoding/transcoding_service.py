8e6b961a72df010103d838db1b0d3cab3a4c5236
DevanshYadav0702 <devanshyadav0702@gmail.com>
2023-10-30 15:21:37 +0530

    transcoding error fix

--- transcoding/transcoding_service.py
+++ transcoding/transcoding_service.py
@@ -1,4 +1,4 @@
 import os
-def convert_file_to_readable_mp3 (local_input_file_path:str,local_ouput_file_path:str):
+def convert_file_to_readable_mp3(local_input_file_path:str,local_ouput_file_path:str):
 
     os.system(f'ffmpeg -i {local_input_file_path} {local_ouput_file_path}')
